3a65f02005cb0405416f9f25e599fd656965c7da
alumno <alumno@PC-V101-15.uacj.mx>
2025-03-05 10:47:16 -0700

    [AGREGA]un poco de estilo

---
+++ Agenda Shufle/Agenda Shufle/pantallas/patanlla_del_ganador.swift
@@ -0,0 +1,18 @@
+//
+//  patanlla_del_ganador.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 3/5/25.
+//
+
+import SwiftUI
+
+struct patanlla_del_ganador: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    patanlla_del_ganador()
+}
